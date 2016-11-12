12c1c4f0ef3cc300b3f26bac15000c3e42a13d2b
oshnix <sdsdaniil@gmail.com>
2016-11-12 14:15:51 +0300

    project structure changes

--- interpretator.c
+++ interpretator.c
@@ -1,6 +1,64 @@
-//
-// Created by danya on 12.11.16.
-//
+#include <stdio.h>
+#include <malloc.h>
+#include <stdlib.h1>
+
+char *buffer;
+char *variables[];
+int *variableValues;
+int variablesCount;
+
+
+void readWord(char* word){
+    while(*buffer && *buffer != ' ' && *buffer != '\n') *(word++) = *(buffer++);
+    *(word) = '\0';
+}
+
+int strlen(char *str){
+    int res = 0;
+    while(*(str++)) res++;
+    return res;
+}
+
+void strcopy(char *src, char *res){
+    while(*(res++) = *(src++));
+}
+
+int addNewVariable(char *word){
+    variables[variablesCount] = malloc(strlen(word)+1);
+    strcopy(word, variables[variablesCount]);
+    return variablesCount++;
+}
+
+char equal(char *left, char *right){
+    while(*left != '\0' && *right != '\0' && *(left++) == *(right++));
+    if(*left == '\0' && *right == '\0') return 1;
+    else return 0;
+}
+
+void interpretateNextWord(char *word){
+    if(equal(word, "if")){
+
+    } else if(equal(word, "goto")){
+
+    } else if(equal(word, "print")){
+
+    } else if(equal(word, "end")){
+
+    } else{
+        int a = isVariable(word);
+        if(a == -1) a = addNewVariable(word);
+    }
+
+}
+
+int isVariable(char *word){
+    for(int i = 0; i < variablesCount; i++){
+        if(equal(word, variables[i])){
+            return i;
+        }
+    }
+    return -1;
+}
 
 
 FILE* initProc(char* filename){
@@ -9,7 +67,14 @@ FILE* initProc(char* filename){
 }
 
 void executeNextCommand(FILE* fin){
-    
+    size_t len = 0;
+    getline(&buffer, &len, fin);
+    char *word = malloc(51);
+    readWord(word);
+    interpretateNextWord(word);
+
+
+
 }
 
 
@@ -20,6 +85,11 @@ int main(int argc, char *argv[]){
         printf("Invalid file");
         exit(1);
     }
+    else{
+        buffer = malloc(256);
+
+    }
+
     while(1){
         executeNextCommand(program);
     }
