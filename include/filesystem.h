f9e4cacbf7fbede4e6ca4e86a1cd0f5c7b686588
oshnix <sdsdaniil@gmail.com>
2016-11-14 23:26:58 +0300

    rewrited pointer work and some more examples were added

--- include/filesystem.h
+++ include/filesystem.h
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <malloc.h>
+#include <string.h>
 
 extern int maxInode = 0;
 
--- src/filesystem.c
+++ src/filesystem.c
@@ -1,36 +1,61 @@
 #include "filesystem.h"
 
+
 void addFile(file *parent, file *child){
     if(parent->actualSize == parent->usedSize){
         parent->actualSize += DEFAULT_INCREASE;
-        parent->content = realloc(parent->content, parent->actualSize * sizeof(file));
+        parent->content = realloc(parent->content, parent->actualSize);
     }
     *((file**)(parent->content + parent->usedSize)) = child;
     ++parent->usedSize;
 }
 
+void reWriteContent(file *regularFile, char *content, size_t content_len){
+    regularFile->content = malloc(content_len+1);
+    strncpy((char*)regularFile->content, content, content_len);
+    regularFile->actualSize = regularFile->usedSize = content_len;
+}
 
-file newFile(file *parent, char *filename, char type){
-    file newFile = {parent, maxInode, filename, type, 0, 0, NULL};
+void addContent(file *regularFile, char *content, size_t content_len){
+    regularFile->content = realloc(regularFile->content, regularFile->actualSize + content_len + 1);
+    strncpy((char*)(regularFile->content + regularFile->actualSize - 1), content, content_len);
+    regularFile->actualSize = regularFile->usedSize = regularFile->actualSize + content_len;
+}
+
+
+file* newFile(file *parent, char *filename, char type){
+    file *newFile = malloc(sizeof(file));
+    newFile->parent = parent;
+    newFile->inode = maxInode;
     ++maxInode;
+    newFile->name = filename;
+    newFile->type = type;
+    newFile->actualSize = 0;
+    newFile->usedSize = 0;
+    newFile->content = NULL;
     if(parent != NULL){
-        addFile(parent, &newFile);
+        addFile(parent, newFile);
     }
     return newFile;
 }
 
 
-file initFileSystem(){
-    file home = newFile(NULL, "home", 'd');
-    home.parent = &home;
+file* initFileSystem(){
+    file *home = newFile(NULL, "home", 'd');
+    home->parent = home;
     return home;
 }
 
 
 int main(){
-    file home = initFileSystem();
-    file profile = newFile(&home, "profile", '-');
-    file *prof = *(file**)home.content;
-    printf("%p %d %d %d\n",prof->parent, prof->inode, prof->actualSize, prof->usedSize);
+    file *home = initFileSystem();
+    file *profile = newFile(home, "profile", '-');
+    char hello[] = "Hello, world!";
+    char moreHel[] ="\nHalLo";
+    file *prof = *(file**)home->content;
+    printf("Prof: %d %d %d %s\n", prof->inode, prof->actualSize, prof->usedSize, (char*)prof->content);
+    reWriteContent(prof, hello, sizeof(hello));
+    addContent(profile, moreHel, sizeof(moreHel));
+    printf("Prof: %d %d %d %s\n", profile->inode, profile->actualSize, profile->usedSize, (char*)profile->content);
     return 0;
 }
